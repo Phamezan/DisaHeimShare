d53c6a0b0789f95bdadf164238382862983ab921
Jonathan L. <70529549+Hairo03@users.noreply.github.com>
2024-10-10 09:22:52 +0200

    Initial commit

---
+++ Disaheim/Amulet.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disaheim
+{
+    public class Amulet
+    {
+        public string ItemId { get; set; }
+        public string Design {  get; set; }
+        public Level Quality { get; set; }
+        
+
+        public Amulet(string itemId) : this (itemId, Level.medium, "")
+        {
+        }
+
+        public Amulet(string itemId, Level quality) : this (itemId, quality, "")
+        {
+        }
+
+        public Amulet(string itemId, Level quality, string design)
+        {
+            this.ItemId = itemId;
+            this.Quality = quality;
+            this.Design = design;
+        }
+
+        public override string ToString()
+        {
+            return $"ItemId: {ItemId}, Quality: {Quality}, Design: {Design}";
+        }
+    }
+}
---
+++ Disaheim/Book.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disaheim
+{
+    public class Book
+    {
+        public string ItemId { get; set; }
+        public string Title { get; set; }
+        public double Price { get; set; }
+
+
+        public Book(string itemId, string title, double price)
+        {
+            this.ItemId = itemId;
+            this.Title = title;
+            this.Price = price;
+        }
+
+        public Book(string itemId,string title) : this (itemId, title, 0)
+        {
+        }
+        public Book(string itemId) : this (itemId, "", 0)
+        {
+        }
+
+        public override string ToString()
+        {
+            return $"ItemId: {ItemId}, Title: {Title}, Price: {Price.ToString(CultureInfo.CreateSpecificCulture("da-DA"))}";
+        }
+    }
+}
